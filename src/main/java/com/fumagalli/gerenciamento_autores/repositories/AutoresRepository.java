a7f76ee519304920d7440be68886f227c03ef614
gustavo-fumagalli <g.fumagalli006@gmail.com>
2026-03-24 11:27:23 -0300

    repository criado

---
+++ src/main/java/com/fumagalli/gerenciamento_autores/repositories/AutoresRepository.java
@@ -0,0 +1,6 @@
+package com.fumagalli.gerenciamento_autores.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutoresRepository extends JpaRepository<AutoresRepository, Long> {
+}
